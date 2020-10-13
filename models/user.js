5a01d630da09fbc7a8a5e0c0807cf5ff37ab1096
prerna <bhatraprerna061@gmail.com>
2020-10-13 09:58:15 +0530

    npm install

--- models/user.js
+++ models/user.js
@@ -1,6 +1,7 @@
 const mongoose=require("mongoose");
 const crypto=require('crypto');
-const { v1: uuid } = require('uuid');
+const {v1:uuidv1}=require("uuid")
+
 const userSchema=new mongoose.Schema({
 	name:
 	{
